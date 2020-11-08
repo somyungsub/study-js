2d63628a4859a1e763098af729fa35d77ac6749f
somyungsub <gkdldy5@gmail.com>
2020-11-08 22:31:56 +0900

    Add : 6.프로토타입 - constructor

--- book/코어자바스크립트/chap06.js
+++ book/코어자바스크립트/chap06.js
@@ -43,3 +43,74 @@ console.dir(instance);
 
 console.log(Constructor.prototype.getName());
 console.log(instance.getName());
+
+
+// constructor
+console.log("====================");
+var NewConstructor = function () {
+  console.log('this is new constructor!');
+};
+
+var dataTypes = [
+  1,
+  'test',
+  true,
+  {},
+  [],
+  function () {
+  },
+  /test/,
+  new Number(),
+  new String(),
+  new Boolean(),
+  new Object(),
+  new Array(),
+  new Function(),
+  new RegExp(),
+  new Date(),
+  new Error()
+];
+
+dataTypes.forEach(function (d) {
+  d.constructor = NewConstructor;
+  console.log(d.constructor.name, '&', d instanceof NewConstructor);
+});
+
+console.log("================");
+var Person2 = function (name) {
+  this.name = name;
+};
+
+var p1 = new Person2('사람1');
+var p1Proto = Object.getPrototypeOf(p1);
+var p2 = new Person2.prototype.constructor('사람2');
+var p3 = new p1Proto.constructor('사람3');
+var p4 = new p1.__proto__.constructor('사람4');
+var p5 = new p1.constructor('사람5');
+
+[p1, p2, p3, p4, p5].forEach(function (p) {
+  console.log(p, p instanceof Person2);
+});
+
+console.log(p1Proto);
+
+Person2.prototype.getName = function () {
+  return this.name;
+};
+
+var iu = new Person2('지금');
+
+// 오버라이드
+iu.getName = function () {
+  return '바로 ' + this.name;
+};
+
+console.log(iu.getName());  // 바로 지금
+console.log(p1.getName());  // 사람1
+console.log(iu.__proto__.getName());  // undefined
+
+iu.__proto__.name = '이 지금!';
+
+console.log(iu.__proto__.getName());        // 이 지금!
+console.log(iu.__proto__.getName.call(iu)); // 지금
+
