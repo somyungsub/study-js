c435021c9c7de17a633dab1fd3bbcf877e4acf31
somyungsub <gkdldy5@gmail.com>
2020-11-27 11:08:21 +0900

    Add : 5. 클로저

--- book/코어자바스크립트/chap05_클로저.js
+++ book/코어자바스크립트/chap05_클로저.js
@@ -109,8 +109,8 @@
 // })();
 
 // 5-6 콜백함수와 클로저
-var fruits = ['apple', 'banana', 'peach'];
-var $ul = document.createElement('ul');   // 공통코드
+// var fruits = ['apple', 'banana', 'peach'];
+// var $ul = document.createElement('ul');   // 공통코드
 
 // 1
 // fruits.forEach(function (fruit) {                     // A
@@ -124,9 +124,9 @@ var $ul = document.createElement('ul');   // 공통코드
 // document.body.appendChild($ul);
 
 // 2
-var alertFruit = function (fruit) {
-  alert('your choice is ' + fruit);
-};
+// var alertFruit = function (fruit) {
+//   alert('your choice is ' + fruit);
+// };
 //
 // fruits.forEach(function (fruit) {
 //   var $li = document.createElement('li');
@@ -150,16 +150,186 @@ var alertFruit = function (fruit) {
 // alertFruit(fruits[1]);
 
 // 4 고차함수 이용
-var alertFruitBuilder = function (fruit) {
+// var alertFruitBuilder = function (fruit) {
+//   return function () {
+//     alert('your choice is ' + fruit);
+//   };
+// };
+//
+// fruits.forEach(function (fruit) {
+//   var $li = document.createElement('li');
+//   $li.innerText = fruit;
+//   $li.addEventListener('click', alertFruitBuilder(fruit));
+//   $ul.appendChild($li);
+// });
+// document.body.appendChild($ul);
+
+// 정보 은닉화
+// var outer = function () {
+//   var a = 1;
+//   var inner = function () {
+//     return ++a;
+//   };
+//   return inner;
+// };
+//
+// var outer2 = outer();
+// console.log(outer2());
+// console.log(outer2());
+
+// 간단한 게임을 통한 정보 은닉
+// var car = {
+//   fuel: Math.ceil(Math.random() * 10 + 10), // 연료(L)
+//   power: Math.ceil(Math.random() * 3 + 2),  // 연비(km/L)
+//   moved: 0, // 총이동거리
+//   run: function () {
+//     var km = Math.ceil(Math.random() * 6);
+//     var wasteFuel = km / this.power;
+//     if (this.fuel < wasteFuel) {
+//       console.log("이동불가");
+//       return;
+//     }
+//
+//     this.fuel -= wasteFuel;
+//     this.moved += km;
+//     console.log(km + 'km 이동 (총 ' + this.moved + 'km)');
+//   }
+// };
+
+// 마음대로 변경이 가능... 은닉x
+// car.fuel = 10000;
+// car.power = 1000;
+// car.moved = 1000;
+
+// 클로저를 활용하여 은닉
+/*var createCar = function () {
+  // private
+  var fuel = Math.ceil(Math.random() * 10 + 10); // 연료(L)
+  var power = Math.ceil(Math.random() * 3 + 2);  // 연비(km/L)
+  var moved = 0; // 총이동거리
+
+  // public
+  // return {
+  //   get moved() {
+  //     return moved;
+  //   },
+  //   run: function () {
+  //     var km = Math.ceil(Math.random() * 6);
+  //     var wasteFuel = km / power;
+  //     if (fuel < wasteFuel) {
+  //       console.log("이동불가");
+  //       return;
+  //     }
+  //
+  //     fuel -= wasteFuel;
+  //     moved += km;
+  //     console.log(km + 'km 이동 (총 ' + moved + 'km. 남은 연료:' + fuel);
+  //   }
+  // }
+
+  // public2
+  var publicMembers = {
+    get moved() {
+      return moved;
+    },
+    run: function () {
+      var km = Math.ceil(Math.random() * 6);
+      var wasteFuel = km / power;
+      if (fuel < wasteFuel) {
+        console.log("이동불가");
+        return;
+      }
+
+      fuel -= wasteFuel;
+      moved += km;
+      console.log(km + 'km 이동 (총 ' + moved + 'km. 남은 연료:' + fuel);
+    }
+  };
+
+  Object.freeze(publicMembers);
+  return publicMembers;
+};*/
+
+// var carCrate = createCar();
+// carCrate.run();
+// console.log(carCrate.moved);
+// console.log(carCrate.fuel);
+// console.log(carCrate.power);
+//
+// carCrate.fuel = 1000;
+// console.log(carCrate.fuel);
+// carCrate.run();
+//
+// carCrate.power = 100;
+// console.log(carCrate.power);
+// carCrate.run();
+//
+// carCrate.moved = 1000;
+// console.log(carCrate.moved);
+// carCrate.run();
+
+// 5-13 bind 메서드 활용 : 부분적용함수
+var add = function () {
+  var result = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+  return result;
+};
+
+var addPartial = add.bind(null, 1, 2, 3, 4, 5); // func 반환
+console.log(addPartial(6, 7, 8, 9, 10));
+console.log(addPartial(6, 7, 8));
+
+// 부분적용함수 2
+console.log("======= 부분적용2 =======");
+var partial = function () {
+  var originalPartialArgs = arguments;
+  var func = originalPartialArgs[0];
+  if (typeof func !== 'function') {
+    throw new Error('첫번째 인자가 함수가 아닙니다');
+  }
+
   return function () {
-    alert('your choice is ' + fruit);
+    var partialArgs = Array.prototype.slice.call(originalPartialArgs, 1);
+    var restArgs = Array.prototype.slice.call(arguments);
+
+    return func.apply(this, partialArgs.concat(restArgs));
   };
 };
 
-fruits.forEach(function (fruit) {
-  var $li = document.createElement('li');
-  $li.innerText = fruit;
-  $li.addEventListener('click', alertFruitBuilder(fruit));
-  $ul.appendChild($li);
-});
-document.body.appendChild($ul);
+var addPartial2 = partial(add, 1, 2, 3, 4, 5);
+console.log(addPartial2(6, 7, 8, 9, 10));
+console.log(addPartial2(6, 7, 8));
+
+var dog = {
+  name: '강아지',
+  greet: partial(function (prefix, suffix) {
+    return prefix + this.name + suffix;
+  }, '왈왈, ')
+};
+
+console.log(dog.greet("입니다!"));
+
+
+// 5-16 디바운스
+var debounce = function (eventName, func, wait) {
+  var timeoutId = null;
+  return function (event) {
+    var self = this;
+    console.log(eventName, 'event 발생');
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(func.bind(self, event), wait);
+  };
+};
+
+var moveHandler = function (e) {
+  console.log('move evnet 처리')
+};
+
+var wheelHandler = function (e) {
+  console.log('wheel evnet 처리');
+};
+
+document.body.addEventListener('mousemove', debounce('move', moveHandler, 500));
+document.body.addEventListener('mousewheel', debounce('wheel', wheelHandler, 700));
