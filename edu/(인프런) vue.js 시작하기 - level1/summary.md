a6845695b4673db4d63284ad665dfc6ae67f346e
somyungsub <gkdldy5@gmail.com>
2020-11-29 13:59:05 +0900

    form 작성 / 마무리

--- edu/(인프런) vue.js 시작하기 - level1/summary.md
+++ edu/(인프런) vue.js 시작하기 - level1/summary.md
@@ -8,4 +8,16 @@
 - Auto Close Tag
 - Atom Keymap
 
-## vue 는
+## 정리
+- Reactivity
+- 인스턴스
+- 컴포넌트
+- 컴포넌트 통신
+  - props (상위 -> 하위 : data)
+  - event emit (하위 -> 상위 : event)
+- HTTP 통신 라이브러리 -> `axios`
+- 템플릿 문법
+  - 데이터 바인딩
+  - 뷰 디렉티브 (v-)
+- `Vue CLI` -> 주로 사용하게됨
+- 싱글 파일 컴포넌트 (.vue 파일)
---
+++ edu/(인프런) vue.js 시작하기 - level1/vue-form/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+<!--    <button type="submit" v-on:click="submitForm">login</button> &lt;!&ndash; 메서드 구현해서 해도되나.. form에서 제어 &ndash;&gt;-->
+    <button type="submit" >login</button> <!-- 메서드 구현해서 해도되나.. form에서 제어 -->
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function () { // 컴포넌트간의 공유되지 않게 새객체를 리턴하도록 구성
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function (event) {
+      // event.preventDefault(); // 새로고침을 하지 않게 됨 (버블링 방지)
+      console.log(this.username, this.password);
+      console.log(event);
+
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      // host(서버)로 전송
+      axios.post(url, data)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+  }
+}
+</script>
+
+<style>
+</style>
