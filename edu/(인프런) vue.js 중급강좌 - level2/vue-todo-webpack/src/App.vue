f231de12ad230fbc7abdcc1bedde4794d8efbe42
somyungsub <gkdldy5@gmail.com>
2020-11-30 21:53:03 +0900

    TODO 만들기

---
+++ edu/(인프런) vue.js 중급강좌 - level2/vue-todo-webpack/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TodoHeader/>
+    <TodoInput/>
+    <TodoList/>
+    <TodoFooter/>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import TodoFooter from './components/TodoFooter';
+
+export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+};
+</script>
+
+<style>
+</style>
