73a4f5ed7de7227d3871da6fd62626af1f5f5b17
somyungsub <gkdldy5@gmail.com>
2025-04-28 22:33:10 +0900

    async await 실행흐름 테스트 추가

---
+++ test/function/async-test5.js
@@ -0,0 +1,20 @@
+async function callApi() {
+  console.log("test1");
+  await Promise.resolve().then(value => console.log('promise'));
+  await console.log('await!');
+  console.log("test2");
+}
+
+async function search() {
+  console.log("start~");
+  await callApi();
+  console.log("test3");
+  console.log("============");
+
+  for (let i = 0; i < 1000; i++) {
+    console.log(`i : ${i}`);
+  }
+  console.log("end~");
+}
+
+search();
