b07c381f58cf2f7aebf7b826b508315647992d59
somyungsub <gkdldy5@gmail.com>
2025-05-12 02:40:00 +0900

    내용 수정

--- test/function/async-test3.js
+++ test/function/async-test3.js
@@ -1,14 +1,20 @@
 async function callApi() {
-  console.log("test1");
-  await console.log('await!');
+  console.log("callApi 1");
+  await setData('data'); // 실은 동기 함수라 await 의미 없음
+  console.log("callApi 2");
   await Promise.resolve().then(value => console.log('promise'));
-  console.log("test2");
+  console.log("callApi 3");
+}
+
+// async만 붙어 있고 실제 비동기 작업 없음
+async function setData(data) {
+  console.log("setData : ", data);
 }
 
 async function search() {
   console.log("start~");
   await callApi();
-  console.log("test3");
+  console.log("call 종료");
   console.log("============");
 
   for (let i = 0; i < 1000; i++) {
