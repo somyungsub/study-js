9d39b2de9bce5d1e351d27725d49da850a2c26fa
somyungsub <gkdldy5@gmail.com>
2025-05-20 04:28:13 +0900

    tick, watch 테스트

--- vue/test/vite-project/src/views/watch/WatchTest.vue
+++ vue/test/vite-project/src/views/watch/WatchTest.vue
@@ -1,29 +1,68 @@
 <script setup>
-import {onMounted, ref, watch} from 'vue';
+import {nextTick, onMounted, ref, watch} from 'vue';
 
 const _state = {
   count: ref(0)
 };
 
-console.log('1. watch 등록 전');
+console.log('watch 등록 전');
 
 watch(_state.count, (newVal, oldVal) => {
-  console.log('3. watch 콜백 실행', newVal, oldVal);
+  console.log('watch 콜백 실행', newVal, oldVal);
 });
 
-console.log('2. count 변경 전');
-
+console.log('count 변경 전');
 _state.count.value++;
-
-console.log('4. count 변경 후');
+console.log('count 변경 후');
 
 function test() {
-  console.log('=========== test 변경 전 ');
-  _state.count.value++;
-  console.log('=========== test 변경 후 ');
+  console.log('=========== test start ');
+  Promise.resolve().then(() => {
+    console.log("promise");
+  })
+  nextTick(() => {
+    console.log("전");
+  })
+  _state.count.value++; // 변경 nextTick 보다 먼저 실행됨 flushJob(vue가 내부적으로 nextTick 보다먼저 실행시킴)
+  nextTick(() => {
+    console.log("후");
+  })
+  console.log('=========== test end ');
 }
+
+
+async function asyncTest() {
+  console.log('=========== test start ');
+  await Promise.resolve().then(() => {
+    console.log("promise");
+  })
+  // tick2 (tich 분리 await로 인함)
+  console.log("promise 후");
+  // 실행순서 우선순위 아닌 -> 경쟁 -> 등록순으로 진행되는듯(기본적으로? 단언은 x)
+  nextTick(() => {
+    console.log("전");
+  })
+  _state.count.value++; // 변경
+  nextTick(() => {
+    console.log("후");
+  })
+  console.log('=========== test end ');
+}
+
+async function testTick() {
+  console.log('=========== test start ');
+  _state.count.value++; // 변경 tick1
+  _state.count.value++; // 변경 tick1 -> 같은팀 flush 한번에 최적화
+  console.log('=========== test end ');
+  setTimeout(() => {
+    _state.count.value++; // 변경 tick2 다른 tick -> watch 한번 더 실행
+  })
+}
+
 onMounted(() => {
-  test();
+  // test();
+  // asyncTest();
+  testTick();
 })
 </script>
 
