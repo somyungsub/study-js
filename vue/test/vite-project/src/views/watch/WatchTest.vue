aa0273d677e2b396f50f39e33cd3c7f051b14900
somyungsub <gkdldy5@gmail.com>
2025-05-20 03:23:14 +0900

    와치테스트 추가

---
+++ vue/test/vite-project/src/views/watch/WatchTest.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {onMounted, ref, watch} from 'vue';
+
+const _state = {
+  count: ref(0)
+};
+
+console.log('1. watch 등록 전');
+
+watch(_state.count, (newVal, oldVal) => {
+  console.log('3. watch 콜백 실행', newVal, oldVal);
+});
+
+console.log('2. count 변경 전');
+
+_state.count.value++;
+
+console.log('4. count 변경 후');
+
+function test() {
+  console.log('=========== test 변경 전 ');
+  _state.count.value++;
+  console.log('=========== test 변경 후 ');
+}
+onMounted(() => {
+  test();
+})
+</script>
+
+<template>
+  {{_state.count}}
+</template>
+
+<style scoped>
+
+</style>
